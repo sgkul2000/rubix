2ae3d11f9295e8b8c7e87bb5a0a67c6b4c189b52
Shreesh Kulkarni <sgkul2000@gmail.com>
2021-07-30 21:29:16 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.pug", "./src/*.pug", "./src/**/*.js"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
